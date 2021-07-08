35a440e644e3ce275375c8041d82e75c93764ea1
Wen Wang <wen.wang@adgeek.com.tw>
2021-07-08 18:40:46 +0800

    wirte original method to show demo

--- src/components/ButtonClickAction.js
+++ src/components/ButtonClickAction.js
@@ -1,7 +1,34 @@
 import React from "react";
-import Hoc from '../hoc/withHighOrderComponent';
-const ButtonClickAction = (props) => (
-  <button className="button--cancel" onClick={props.increase}>Btn clicks count: {props.counter}</button>
-)
+// import Hoc from '../hoc/withHighOrderComponent';
+// const ButtonClickAction = (props) => (
+//   <button className="button--cancel" onClick={props.increase}>Btn clicks count: {props.counter}</button>
+// )
 
-export default Hoc(ButtonClickAction);
+// export default Hoc(ButtonClickAction);
+
+  class ButtonClickAction extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        counter: 0
+      }
+      this.increaseCounter=this.increaseCounter.bind(this)
+    }
+
+    increaseCounter() {
+      this.setState(
+        prevState => ({
+          counter: prevState.counter + 1
+        })
+      )
+    }
+
+    render() {
+      return (
+        <button className="button--cancel" onClick={() => this.increaseCounter()}>Btn clicks count: {this.state.counter}</button>
+      )
+    }
+  }
+
+
+export default ButtonClickAction;
--- src/components/ButtonMoveAction.js
+++ src/components/ButtonMoveAction.js
@@ -1,7 +1,34 @@
 import React from "react";
-import Hoc from '../hoc/withHighOrderComponent';
-const ButtonMoveAction = (props) => (
-  <button className="button--accept" onMouseOut={props.increase}>Mouse out count: {props.counter}</button>
-)
+// import Hoc from '../hoc/withHighOrderComponent';
+// const ButtonMoveAction = (props) => (
+//   <button className="button--accept" onMouseOut={props.increase}>Mouse out count: {props.counter}</button>
+// )
 
-export default Hoc(ButtonMoveAction);
+// export default Hoc(ButtonMoveAction);
+
+class ButtonMoveAction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+    this.increaseCounter=this.increaseCounter.bind(this)
+  }
+
+  increaseCounter() {
+    this.setState(
+      prevState => ({
+        counter: prevState.counter + 1
+      })
+    )
+  }
+
+  render() {
+    return (
+      <button className="button--accept" onMouseOut={() => this.increaseCounter()}>Mouse out count: {this.state.counter}</button>
+    )
+  }
+}
+
+
+export default ButtonMoveAction;
