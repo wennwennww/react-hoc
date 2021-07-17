1dc4d110848d938092e58ebfad573d694db4ac14
Wen Wang <wen.wang@adgeek.com.tw>
2021-07-17 20:54:50 +0800

    Modified comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ButtonClickAction from './components/ButtonClickAction';
 import ButtonMoveAction from './components/ButtonMoveAction';
 
-// 兩個button,一個點擊可以count++,一個滑鼠滑出後才count++
 const App = () => (
   <div>
     <ButtonClickAction />
